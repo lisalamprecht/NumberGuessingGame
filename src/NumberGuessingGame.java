028f623ae3a1bdceb0dd81602df9466896959a30
lisalamprecht <59170267+lisalamprecht@users.noreply.github.com>
2020-12-28 15:53:57 +0000

    Update NumberGuessingGame.java

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -5,14 +5,8 @@ package src;
  * Author: Lisa-Maria Lamprecht
  */
 public class NumberGuessingGame {
-
     public static void main(String args[]) {
-
         //create an instance of the frame
-        MyFrame frame = new MyFrame(); 
-         
-
-        
+        MyFrame frame = new MyFrame();    
     }
-
 }
